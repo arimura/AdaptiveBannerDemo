8118e45d50af311a12673bed1811c39fab30c861
arimura <kotaro_arimura@voyagegroup.com>
2022-04-13 22:05:01 +0900

    Refactor

--- AdaptiveBannerDemo/ViewController.swift
+++ AdaptiveBannerDemo/ViewController.swift
@@ -11,9 +11,15 @@ import GoogleMobileAds
 class ViewController: UIViewController, GADBannerViewDelegate{
     
     var bannerView :GADBannerView!
-
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        initAndLoadBanner()
+    }
+    
+    func initAndLoadBanner(){
         // Step 1: Create an inline adaptive banner ad size. This size is used to
         // request your adaptive banner. You can pass in the width of the device, or set
         // your own width. This example sets a static width.
@@ -23,38 +29,44 @@ class ViewController: UIViewController, GADBannerViewDelegate{
         bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
         bannerView.rootViewController = self
         bannerView.delegate = self
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(bannerView)
         self.bannerView = bannerView
         
-
         // Step 3: Load an ad.
         let request = GADRequest()
         bannerView.load(request)
+        
     }
     
     func bannerViewDidReceiveAd(_ bannerView: GADBannerView) {
-      print("bannerViewDidReceiveAd")
+        print("bannerViewDidReceiveAd")
+        self.contentView.addSubview(bannerView)
+        bannerView.widthAnchor.constraint(equalToConstant: 320).isActive = true
+        bannerView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        bannerView.topAnchor.constraint(equalTo: self.contentView.topAnchor).isActive = true
+        bannerView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor).isActive = true
     }
-
+    
     func bannerView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: Error) {
-      print("bannerView:didFailToReceiveAdWithError: \(error.localizedDescription)")
+        print("bannerView:didFailToReceiveAdWithError: \(error.localizedDescription)")
     }
-
+    
     func bannerViewDidRecordImpression(_ bannerView: GADBannerView) {
-      print("bannerViewDidRecordImpression")
+        print("bannerViewDidRecordImpression")
     }
-
+    
     func bannerViewWillPresentScreen(_ bannerView: GADBannerView) {
-      print("bannerViewWillPresentScreen")
+        print("bannerViewWillPresentScreen")
     }
-
+    
     func bannerViewWillDismissScreen(_ bannerView: GADBannerView) {
-      print("bannerViewWillDIsmissScreen")
+        print("bannerViewWillDIsmissScreen")
     }
-
+    
     func bannerViewDidDismissScreen(_ bannerView: GADBannerView) {
-      print("bannerViewDidDismissScreen")
+        print("bannerViewDidDismissScreen")
     }
 }
 
